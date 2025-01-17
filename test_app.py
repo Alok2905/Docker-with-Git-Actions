e01625061e90e34fed3339b53ba4889afe3d1259
Alok Shinde <87236610+Alok2905@users.noreply.github.com>
2025-01-17 17:31:26 +0530

    test file updated

--- test_app.py
+++ test_app.py
@@ -6,4 +6,4 @@ def test_home():
     response = app.test_client().get("/")
 
     assert response.status_code == 200
-    assert response.data== f'Hello Alok'
+    assert response.data== b'Hello Alok'
